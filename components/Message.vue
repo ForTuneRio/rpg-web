4d602595babb5e7295b7e3f3e5b29d91bd7c922f
Ge-Schoka <denis.bacumenco@kochfreiburg.de>
2024-11-04 09:41:47 +0100

    styling

--- components/Message.vue
+++ components/Message.vue
@@ -1,34 +1,43 @@
 <template>
   <div
-    class="flex gap-1 mb-4"
+    class="flex mb-4"
     :class="{
-      'justify-end': isUser,  // Align user messages to the right
-      'justify-start': !isUser // Align chat messages to the left
+      'justify-end': isUser,
+      'justify-start': !isUser
     }"
   >
-    <!-- Bot message: show icon and title -->
-    <div
-      v-if="!isUser"
-      class="flex items-center gap-2"
-    >
-      <img
-        src="../assets/chatgpt.svg"
-        alt="chatgpt"
-        class="w-4 h-4 mt-1"
-      />
-      <div class="text-sm font-bold">ChatRPG</div>
-    </div>
+    <!-- Assistant Messages -->
+    <template v-if="!isUser">
+      <div class="flex items-start gap-2">
+        <!-- Icon and title -->
+        <div class="flex flex-col items-center pt-2">
+          <img
+            src="../assets/chatgpt.svg"
+            alt="chatgpt"
+            class="w-6 h-6"
+          />
+          <div class="text-sm font-bold">ChatRPG</div>
+        </div>
+        <!-- Message content -->
+        <div
+          class="bg-gray-100 text-black p-2 rounded-lg max-w-xl break-words"
+        >
+          {{ content }}
+        </div>
+      </div>
+    </template>
 
-    <!-- Message content -->
-    <div
-      :class="{
-        'bg-blue-500 text-white': isUser, // User message style
-        'bg-gray-100 text-black': !isUser // Bot message style
-      }"
-      class="p-2 rounded-lg max-w-xs"
-    >
-      {{ content }}
-    </div>
+    <!-- User Messages -->
+    <template v-else>
+      <div class="flex items-start gap-2">
+        <!-- Message content -->
+        <div
+          class="bg-blue-500 text-white p-2 rounded-lg max-w-xl break-words"
+        >
+          {{ content }}
+        </div>
+      </div>
+    </template>
   </div>
 </template>
 
